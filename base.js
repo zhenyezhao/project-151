c5bb9b3036fee447ddc160830f1621fbfea128bf
zhenyezhao <86699069+zhenyezhao@users.noreply.github.com>
2023-02-12 19:28:20 -0800

    Add files via upload

---
+++ base.js
@@ -0,0 +1,13 @@
+AFRAME.registerComponent('base')
+schema:{
+    radius:{type:'number' ,default: 150 },
+    height:{type:'number' ,default: 3 }
+},
+
+init: function(){
+this.el.setAttribute('geometry'
+primitive:'cylinder',
+radius=this.data.radius,
+height=this.data.height
+)};
+this.el.setAttribute(material{color:'#1769aa'})
